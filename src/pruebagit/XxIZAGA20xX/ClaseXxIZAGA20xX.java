9b7d7566a6130ccb52a23084891be8a7eac165e6
Vespertino <Vespertino@08-C5>
2016-04-08 15:50:59 +0100

    Ejercicio 8 del PDF de la PO del GITHUB

---
+++ src/pruebagit/XxIZAGA20xX/ClaseXxIZAGA20xX.java
@@ -0,0 +1,20 @@
+package pruebagit.XxIZAGA20xX;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ClaseXxIZAGA20xX {
+    
+    public ClaseXxIZAGA20xX ()
+    {
+        System.out.print("Hola me llamo XxIZAGA20xX");
+    }
+    
+}
