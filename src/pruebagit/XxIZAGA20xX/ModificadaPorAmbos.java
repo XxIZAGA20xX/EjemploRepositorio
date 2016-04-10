eaa180e4cc33044401e9f11498e3f9f241b4e110
Izaga <Izaga@German>
2016-04-10 18:08:34 +0100

    Modificada por ambos

---
+++ src/pruebagit/XxIZAGA20xX/ModificadaPorAmbos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit.XxIZAGA20xX;
+
+/**
+ *
+ * @author Izaga
+ */
+public class ModificadaPorAmbos {
+    
+public ModificadaPorAmbos ()
+    {
+        System.out.print("Escrito por XxIZAGA20xX");
+    }
+    
+}
